f640ce4a5c0083d0ef8b0dc2b6949d77ea6e6337
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 03:32:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  cart.each do
+  cart.keys.each do
     |item|
     binding.pry
     item[:price] = item[:clearance] ? 0.8 * item[:price] : item[:price]
